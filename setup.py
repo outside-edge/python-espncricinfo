8b997d7c576a840f4b3687d979d5943d88bcf14a
Derek Willis <dwillis@gmail.com>
2015-07-04 11:35:27 -0400

    bumped to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="python-espncricinfo",
-      version="0.1.0",
+      version="0.2.0",
       description="ESPNCricInfo API client",
       license="MIT",
       install_requires=["requests"],
