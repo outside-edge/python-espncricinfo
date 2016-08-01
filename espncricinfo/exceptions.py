dfc99750cd27b66d4e99e7a938e56b34dd64da3f
Derek Willis <dwillis@gmail.com>
2016-07-31 20:42:51 -0400

    added NoMatchFoundError

---
+++ espncricinfo/exceptions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+This module contains various exceptions raised by python-espncricinfo.
+"""
+
+
+class NoMatchFoundError(TypeError):
+    """
+    Exception raised if a match_id is not valid or does not exist.
+    """
+    pass
