d71b571bd35787e95373758e7c8bb9abb0377ec1
Derek Willis <dwillis@gmail.com>
2016-08-09 22:13:25 -0400

    0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="python-espncricinfo",
-      version="0.3",
+      version="0.3.1",
       description="ESPNCricInfo API client",
       license="MIT",
       install_requires=["requests", "bs4", "dateparser"],
