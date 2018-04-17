44eae065995073f147512eda8f030be29a55c43c
Derek Willis <dwillis@gmail.com>
2018-04-17 19:08:01 -0400

    fix tests

--- tests.py
+++ tests.py
@@ -8,13 +8,13 @@ class TestMatchMethods(unittest.TestCase):
         self.match = Match(id)
 
     def test_match_description(self):
-        self.assertEqual(self.match.description(), 'Caribbean Premier League, 6th Match: St Lucia Zouks v Guyana Amazon Warriors at Gros Islet, Jun 26, 2015')
+        self.assertEqual(self.match.description, 'Caribbean Premier League, 6th Match: St Lucia Zouks v Guyana Amazon Warriors at Gros Islet, Jun 26, 2015')
 
     def test_match_match_class(self):
-        self.assertEqual(self.match.match_class(), 'Twenty20')
+        self.assertEqual(self.match.match_class, 'Twenty20')
 
     def test_toss_winner(self):
-        self.assertEqual(self.match.toss_winner(), 'AmWar')
+        self.assertEqual(self.match.toss_winner, '5153')
 
 if __name__ == '__main__':
     unittest.main()
