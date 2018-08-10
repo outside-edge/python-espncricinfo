4fafee3e333990e1b8d5d6c9ed0adcdf29f3d83b
Derek Willis <dwillis@gmail.com>
2018-08-09 21:24:22 -0400

    added methods to Series, including events for a series

--- espncricinfo/series.py
+++ espncricinfo/series.py
@@ -1,4 +1,5 @@
 import requests
+import grequests
 from bs4 import BeautifulSoup
 from espncricinfo.exceptions import MatchNotFoundError, NoSeriesError
 
@@ -10,6 +11,8 @@ class Series(object):
         self.events_url = "http://core.espnuk.org/v2/sports/cricket/leagues/{0}/events".format(str(series_id))
         self.seasons_url = "http://core.espnuk.org/v2/sports/cricket/leagues/{0}/seasons".format(str(series_id))
         self.json = self.get_json(self.json_url)
+        self.seasons = self._get_seasons()
+        self.years = self._get_years_from_seasons()
         if self.json:
             self.name = self.json['name']
             self.short_name = self.json['shortName']
@@ -17,13 +20,7 @@ class Series(object):
             self.slug = self.json['slug']
             self.is_tournament = self.json['isTournament']
             self.url = self.json['links'][0]['href']
-            self.season_json = self._get_season()
             self.events_json = self._get_events()
-
-        if self.season_json:
-            self.year = self.season_json['year']
-            self.start_date = self.season_json['startDate']
-            self.end_date = self.season_json['endDate']
 
         if self.events_json:
             self.events = self._build_events()
@@ -35,23 +32,42 @@ class Series(object):
         else:
             return r.json()
 
+    def get_events_for_season(self, season):
+        responses = []
+        season_events = []
+        season_events_url = self.seasons_url+"/{0}/events".format(str(season))
+        season_events_json = self.get_json(season_events_url)
+        if season_events_json:
+            rs = (grequests.get(event['$ref']) for event in season_events_json['items'])
+            responses = grequests.map(rs)
+            for response in responses:
+                event_json = response.json()
+                venue_json = self.get_json(event_json['venues'][0]['$ref'])
+                season_events.append({"url": event_json['$ref'], "match_id": event_json['id'], "class": event_json['competitions'][0]['class']['generalClassCard'], "date": event_json['date'], "description": event_json['shortDescription'], "venue_url": event_json['venues'][0]['$ref'], "venue": venue_json['fullName']})
+            return season_events
+        else:
+            return None
+
     def __str__(self):
         return self.name
 
     def __unicode__(self):
         return self.name
 
-    def _get_season(self):
-        season_json = self.get_json(self.json['season']['$ref'])
+    def _get_seasons(self):
+        season_json = self.get_json(self.seasons_url)
         if season_json:
-            return season_json
+            return [x['$ref'] for x in season_json['items']]
         else:
             return None
+
+    def _get_years_from_seasons(self):
+        return [x.split('/')[9] for x in self.seasons]
 
     def _get_events(self):
         events_json = self.get_json(self.events_url)
         if events_json:
-            return events_json['items']
+            return [x for x in events_json['items']]
         else:
             return None
 
