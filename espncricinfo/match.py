11ac4a0e2e094d721049c9d972dda80df19d5d7d
scrambldchannel <41974784+scrambldchannel@users.noreply.github.com>
2019-11-08 15:36:18 +0100

    Revert "replaced if statements with use of dict get"
    This reverts commit 1e7ee2c80f336cca5adb4ab2242e2554d9c2eb9b.

--- espncricinfo/match.py
+++ espncricinfo/match.py
@@ -160,7 +160,8 @@ class Match(object):
 
     # live matches only
     def _current_summary(self):
-        return self.match_json().get('current_summary')
+        if 'current_summary' in self.match_json().keys():
+            return self.match_json()['current_summary']
 
     def _present_datetime_local(self):
         return self.match_json()['present_datetime_local']
@@ -181,7 +182,7 @@ class Match(object):
             return True
 
     def _rain_rule(self):
-        if self.match_json().get('rain_rule') == "1":
+        if 'rain_rule' in self.match_json() and self.match_json()['rain_rule'] == "1":
             return self.match_json()['rain_rule_name']
         else:
             return None
@@ -190,19 +191,34 @@ class Match(object):
         return self.match_json()['start_date_raw']
 
     def _continent(self):
-        return self.match_json().get('continent_name')
+        if 'continent_name' in self.match_json():
+            return self.match_json()['continent_name']
+        else:
+            return None
 
     def _town_area(self):
-        return self.match_json().get('town_area')
+        if 'town_area' in self.match_json():
+            return self.match_json()['town_area']
+        else:
+            return None
 
     def _town_name(self):
-        return self.match_json().get('town_name')
+        if 'town_name' in self.match_json():
+            return self.match_json()['town_name']
+        else:
+            return None
 
     def _town_id(self):
-        return self.match_json().get('town_id')
+        if 'town_id' in self.match_json():
+            return self.match_json()['town_id']
+        else:
+            return None
 
     def _weather_location_code(self):
-        return self.match_json().get('weather_location_code')
+        if 'weather_location_code' in self.match_json():
+            return self.match_json()['weather_location_code']
+        else:
+            return None
 
     def _match_title(self):
         return self.match_json()['cms_match_title']
@@ -220,7 +236,7 @@ class Match(object):
         return self.match_json()['floodlit_name']
 
     def _followon(self):
-        if self.match_json().get('followon') == '1':
+        if 'followon' in self.match_json() and self.match_json()['followon'] == '1':
             return True
         else:
             return False
@@ -256,7 +272,10 @@ class Match(object):
             return None
 
     def _latest_innings_fow(self):
-        return self.json['centre'].get('fow')
+        if 'fow' in self.json['centre'].keys():
+            return self.json['centre']['fow']
+        else:
+            return None
 
     def _team_1(self):
         return self.json['team'][0]
@@ -268,8 +287,11 @@ class Match(object):
         return self._team_1()['team_abbreviation']
 
     def _team_1_players(self):
-        return self._team_1().get('player', [])
-        
+        if 'player' in self._team_1():
+            return self._team_1()['player']
+        else:
+            return []
+
     def _team_1_innings(self):
         if self.json['innings'] == []:
             return None
@@ -304,8 +326,11 @@ class Match(object):
         return self._team_2()['team_abbreviation']
 
     def _team_2_players(self):
-        return self._team_2().get('player', [])
- 
+        if 'player' in self._team_2():
+            return self._team_2()['player']
+        else:
+            return []
+
     def _team_2_innings(self):
         if self.json['innings'] == []:
             return None
