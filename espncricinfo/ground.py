a01942fe589d3cfc3418e67ed71ee842ad2fb26a
scrambldchannel <41974784+scrambldchannel@users.noreply.github.com>
2019-11-13 16:54:58 +0100

    ground atttributes html parsing added

--- espncricinfo/ground.py
+++ espncricinfo/ground.py
@@ -9,7 +9,9 @@ class Ground(object):
         self.url = "http://www.espncricinfo.com/ci/content/ground/{0}.html".format(str(self.cricinfo_id))
         self.json_url = "http://core.espnuk.org/v2/sports/cricket/venues/{0}".format(str(self.cricinfo_id))
 
+        self.parsed_html = self.get_html()
         self.json = self.get_json()
+
         if self.json:
             self.__unicode__ = self._full_name()
             self.short_name = self._short_name()            
@@ -23,13 +25,12 @@ class Ground(object):
             self.country = self._country()
             self.summary = self._summary()
 
-    def get_html(self):
-        r = requests.get(self.url)
-        if not r.ok:
-            raise GroundNotFoundError
-        else:
-            soup = BeautifulSoup(r.text, 'html.parser')
-            return soup.find("div", class_="pnl650T")
+        if self.parsed_html:
+            self.established = self._established()
+            self.floodlights_added = self._floodlights_added()
+            self.end_names = self._end_names()
+            self.home_team = self._home_team()
+            self.other_sports = self._other_sports()
 
     def get_json(self):
         r = requests.get(self.json_url)
@@ -70,3 +71,26 @@ class Ground(object):
 
     def _summary(self):
         return self.address.get('summary')
+
+    def get_html(self):
+        r = requests.get(self.url)
+        if not r.ok:
+            raise GroundNotFoundError
+        else:
+            soup = BeautifulSoup(r.text, 'html.parser').find('div', id= 'ciHomeContentlhs')
+            return soup
+
+    def _established(self):
+        return self.parsed_html.find('div', id = 'stats').find('label', text = 'Established ').next_sibling
+
+    def _floodlights_added(self):
+        return self.parsed_html.find('div', id = 'stats').find('label', text = 'Floodlights ').next_sibling
+
+    def _end_names(self):
+        return self.parsed_html.find('div', id = 'stats').find('label', text = 'End names ').next_sibling
+
+    def _home_team(self):
+        return self.parsed_html.find('div', id = 'stats').find('label', text = 'Home team ').next_sibling
+
+    def _other_sports(self):
+        return self.parsed_html.find('div', id = 'stats').find('label', text = 'Other sports ').next_sibling
