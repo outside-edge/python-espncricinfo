fc761b7d4d3a378ca0c848dca72112157c05df62
Derek Willis <dwillis@gmail.com>
2020-06-14 16:56:46 -0400

    inital replacement for summary; needs actual live matches for testing

--- espncricinfo/summary.py
+++ espncricinfo/summary.py
@@ -1,18 +1,34 @@
+import json
 import requests
+import datetime
+from bs4 import BeautifulSoup
+from espncricinfo.match import Match
 
 class Summary(object):
 
     def __init__(self):
-        self.url = "http://www.espncricinfo.com/netstorage/summary.json"
-        self.json = self.get_json()
-        self.match_ids = self.json['matches'].keys()
-        self.all_matches = self.json['matches'].values()
+        self.url = "https://www.espncricinfo.com/scores"
+        self.html = self.get_html()
+        self.match_ids = self._match_ids()
+        self.matches = self._build_matches()
 
-    def get_json(self):
+    def get_html(self):
         r = requests.get(self.url)
-        return r.json()
+        if r.status_code == 404:
+            raise MatchNotFoundError
+        else:
+            return BeautifulSoup(r.text, 'html.parser')
 
-    def match(self, id):
-        m = self.json['matches'][id]
-        m['url'] = "http://www.espncricinfo.com"+m['url']
-        return m
+    def json(self):
+        try:
+            text = self.html.find_all('script')[13].contents[0]
+            return json.loads(text)
+        except:
+            return None
+
+    def _match_ids(self):
+        matches = self.json['props']['pageProps']['data']['content']['leagueEvents'][0]['matchEvents']
+        return matches
+
+    def _build_matches(self):
+        return [Match(m) for m in self.match_ids]
